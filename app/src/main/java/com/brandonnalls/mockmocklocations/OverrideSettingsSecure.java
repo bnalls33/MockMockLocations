1f804744a19f56767dba1ca89bf3aa3d9fe25e16
Dingjun <fangdingjun@gmail.com>
2016-03-21 09:27:06 +0800

    remove API level condition for Settings.Secure.ALLOW_MOCK_LOCATION

--- app/src/main/java/com/brandonnalls/mockmocklocations/OverrideSettingsSecure.java
+++ app/src/main/java/com/brandonnalls/mockmocklocations/OverrideSettingsSecure.java
@@ -11,28 +11,27 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class OverrideSettingsSecure implements IXposedHookLoadPackage {
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
-        // before API level 18, hook Settings.Secure.ALLOW_MOCK_LOCATION
-        if (Build.VERSION.SDK_INT < 18) {
-            findAndHookMethod("android.provider.Settings.Secure", lpparam.classLoader, "getString",
-                    ContentResolver.class, String.class, new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                    String requested = (String) param.args[1];
-                    if (requested.equals(Settings.Secure.ALLOW_MOCK_LOCATION)) {
-                        param.setResult("0");
-                    }
-                }
-            });
-            return;
-        }
-
-        // after API level 18, hook Location.isFromMockProvider
-        findAndHookMethod("android.location.Location", lpparam.classLoader, "isFromMockProvider",
-                new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                param.setResult(false);
+        // Settings.Secure.ALLOW_MOCK_LOCATION is depprecated at API level 23
+        findAndHookMethod("android.provider.Settings.Secure", lpparam.classLoader, "getString",
+                ContentResolver.class, String.class, new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        String requested = (String) param.args[1];
+                        if (requested.equals(Settings.Secure.ALLOW_MOCK_LOCATION)) {
+                            param.setResult("0");
+                        }
             }
         });
+
+        // at API level 18, the function Location.isFromMockProvider is added
+        if (Build.VERSION.SDK_INT >= 18) {
+            findAndHookMethod("android.location.Location", lpparam.classLoader, "isFromMockProvider",
+                    new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            param.setResult(false);
+                        }
+            });
+        }
     }
 }
